538aafda0d905c8c189874c195a630f2abe233f8
victoriaTamber <victoriatamber06@gmail.com>
2024-02-27 13:19:48 +0100

    commit 2

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <div id="menyNav">
       <!-- meny og navigasjon  -->
       <div class="logoMOKI_img">
-        <img src="../images/MOKI_logo.svg" alt="Logoen til MOKI, grønn søppelkasse">
+        <img src="images/MOKI_logo.svg" height="100px" alt="Logoen til MOKI, grønn søppelkasse">
       </div>
     
       <div class="menyNavn">
@@ -22,7 +22,7 @@
       <!-- navigasjon (css og java script) -->                                                                                                           
       <div class="nav">
         <button onclick="navFunction()" class="navButton">Meny</button>
-          <div id="dropdownMenu" class="navLinks">
+          <div id="dropdownMeny" class="navLinks">
             <a href="../index.html">Hjem</a>
             <a href="produkt.html">Vårt produkt</a>
             <a href="omOss.html">Om oss</a>
@@ -38,12 +38,12 @@
       </div>
     
 
-    <div class="teamImage">
-      <img src="c:\Users\Victoria Tamber\OneDrive - Osloskolen\IMvg2_2023_2024\arsoppgave_VG2\arsoppgave_kode\images\teamBilde.jpg" title="Foto: Victoria" alt="Bilde av seks ansatte i ungdombedriften Moki, til venstre har vi Lavvanya med rosa sjorte, Victoria med blå genser, Una med en brun og mønstrete genser. Til høyre senter har vi Brage med en sort hettegenser og beige bukse, Brage med en svart jaguar hettejakke og helt til høyre har vi Jamal med en brun hettegenser" >
+    <div class="teamBilde">
+      <img src="images\teamBilde.jpg" title="Foto: Victoria" alt="Bilde av seks ansatte i ungdombedriften Moki, til venstre har vi Lavvanya med rosa sjorte, Victoria med blå genser, Una med en brun og mønstrete genser. Til høyre senter har vi Brage med en sort hettegenser og beige bukse, Brage med en svart jaguar hettejakke og helt til høyre har vi Jamal med en brun hettegenser" >
     </div>
 
     <!-- introduksjonst tekst -->
-    <div class="mainTxt">
+    <div class="hovedTekst">
       <p>Moki UB er en ungdomsbedrift fra kuben VGS i Økern. Bedriften har talentfulle og engasjerte ungdommer som ønsker at verden skal være mer miljøvennlig ved å få en bedre oversikt over 
           befolkningens resirkulering. Vi bestemte oss tidlig å komme med en innovasjon. Vi startet med å komme med løsninger på hvordan vi skal få flere til å resirkulere best mulig. Som sakt er det flere i dag som 
           kjøper klokke som viser hvor mye kalorier du mister ved å løpe så mye, det er ikke bare nå, men også før. En av ansatte kommer opp med å la folket se hvor mye de kaster ved hjelp av vekt, sånn at de for en 
@@ -55,43 +55,44 @@
 
 
     <!-- nyhetsbrev -->
-  <div class="newsletter">
-      <div class="newsHeader">
+  <div class="nyhetsbrev">
+      <div class="nyhetOverskr">
         <h3>Nyhetsbrev</h3>
       </div>
 
-      <div class="newsletterTxt">
+      <div class="nyhetsbrevTekst">
         <p>Har du lyst på å få oppdateringer på hva vi 
            gjør i MOKI og hvordan vårt produkt utvikler
            seg? Meld deg inn på vårt nyhetsbrev!</p>
       </div>
     
 
-    <div class="newsletterContent">
+    <div class="nyhetsbrevInnhold">
         <!-- fornavn input -->
-        <div class="firstNameNL">
-          <label for="firstNameInput">Ditt fornavn:</label>
-          <input type="text" id="firstNameInput" name="Navn" placeholder="Fornavn"> 
+      <form action="nyhetsbrevIndex.php">
+        <div class="fornavnNB">
+          <label for="fornavnInput">Ditt fornavn:</label>
+          <input type="text" id="fornavnInput" name="Navn" placeholder="Fornavn"> 
         </div>
         <!-- etternavn input -->
-        <div class="lastNameNL">
-          <label for="lastNameInput">Ditt etternavn:</label>
-          <input type="text" id="lastNameInput" name="Navn" placeholder="Etternavn"> 
+        <div class="etternavnNL">
+          <label for="etternavnInput">Ditt etternavn:</label>
+          <input type="text" id="etternavnInput" name="Navn" placeholder="Etternavn"> 
         </div>
        
         <!-- epost input --> 
-        <div class="epostNL"> 
+        <div class="epostNB"> 
           <label for="epostInput">E-post:</label>
           <input type="text" id="epostInput" name="epost" placeholder="E-post">
         </div> 
       
-        <div class="checkAgreement">
+        <div class="VilkarEnighet">
           <label for="avtalevilkar">Enig i vilkår </label>
           <input type="checkbox">
         </div>
         <!-- abonnent knappe trykk -->
         <button type="submit" onclick="registrNB()" id="registrAbb"> Ja takk! </button>
-      
+      </form>
    
      
     </div>   
