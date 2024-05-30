9435c9bef57fc94b8ef665720fd4718edff3a83d
victoriaTamber <victoriatamber06@gmail.com>
2024-05-30 13:14:53 +0200

    commit 12 outside main

--- back/nyhetsbrevForsideSlett.php
+++ back/nyhetsbrevForsideSlett.php
@@ -6,34 +6,40 @@ include "../db_kobling.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['slettnyhetsbrevBruker'])) {
     $email = $_GET['slettnyhetsbrevBruker'];
-
+    $stmt = mysqli_prepare($conn, $sql);
     // $sql = "SELECT * FROM nyhetsbrevkunde WHERE idnyhetsbrev = ?";
     // $stmt = mysqli_prepare($conn, $sql);
 
 
-    $sql = "DELETE FROM nyhetsbrevkunde WHERE slettnyhetsbrevBruker = ?";
-     $stmt = mysqli_prepare($conn, $sql);
-     mysqli_stmt_bind_param($stmt, "s", $email);
-     mysqli_stmt_execute($stmt);
+  $sql = "DELETE FROM nyhetsbrevkunde WHERE epost = ?";
+    if($stmt) { 
+      mysqli_stmt_bind_param($stmt, "s", $email);
+      mysqli_stmt_execute($stmt);
  
 
-    if (mysqli_stmt_affected_rows($stmt) > 0) {
-        // Deletion successful, redirect back to the newsletter page
-        header("Location: ../brukerSider/forside.php");
-        echo "Nyhetsbrev abonnent slettet."; 
+        if (mysqli_stmt_affected_rows($stmt) > 0) {
+            // Deletion successful, redirect back to the newsletter page
+            header("Location: ../brukerSider/forside.php");
+            echo "Nyhetsbrev abonnent slettet."; 
          
-    } else {
-        // Deletion failed, handle error
-        echo "Error deleting newsletter subscribers.";
-    }
+        } else {
+            // Deletion failed, handle error
+            echo "Error deleting newsletter subscribers.";
+        }
+      // Lukk statement  
+      mysqli_stmt_close($stmt);
  
+    } else {
+        // Invalid request, redirect to homepage or handle appropriately
+        header("Location: ../brukerSider/nyhetsbrev.php"); 
+        exit(); 
+    }
+
 } else {
-    // Invalid request, redirect to homepage or handle appropriately
-    header("Location: ../brukerSider/nyhetsbrev.php"); 
-    exit(); 
-   
-    
-}
+  // Ugyldig forespørsel, omdiriger til nyhetsbrevsiden eller håndter på passende måte
+  header("Location: ../brukerSider/nyhetsbrev.php");
+  exit();
+};
 
 
 
